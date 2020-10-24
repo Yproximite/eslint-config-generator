c41fba054dd259799de51222722eeebfcfe64431
Hugo Alliaume <kocal@live.fr>
2020-10-24 09:36:15 +0200

    test(vue): test vue + typescript using vue-property-decorator

---
+++ __tests__/fixtures/component-typescript.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    Hello world!
+    <transition name="fade">
+      <div v-show="visible">
+        my image: <img src="https://example.com/image.png" />
+      </div>
+    </transition>
+
+    <slot />
+
+    <MyComponent>
+      <template #my-slot="{  data }">
+        {{ data }}
+      </template>
+    </MyComponent>
+
+    <v-component>Hello</v-component>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component} from 'vue-property-decorator'
+
+@Component
+export default class ComponentTypescript extends Vue {
+  visible: boolean = false;
+
+  mounted() {
+    console.log("Hello world!");
+  }
+}
+</script>
--- __tests__/functional.test.ts
+++ __tests__/functional.test.ts
@@ -2,6 +2,14 @@ import { ESLint } from 'eslint';
 import * as path from 'path';
 import { generateConfig } from '../dist';
 
+function createESLint(options: ESLint.Options): ESLint {
+  return new ESLint({
+    cwd: path.join(__dirname, 'fixtures'),
+    ignore: false,
+    ...options,
+  });
+}
+
 function cleanResults(results: ESLint.LintResult[]) {
   return results.map((result) => {
     return {
@@ -15,13 +23,11 @@ function cleanResults(results: ESLint.LintResult[]) {
 
 describe('functional', function () {
   test('basic lint', async function () {
-    const eslint = new ESLint({
+    const eslint = createESLint({
       baseConfig: generateConfig(),
     });
 
-    const results = cleanResults(
-      await eslint.lintText(`console.log("Hello world!")`, { filePath: path.join(__dirname, 'file.js') })
-    );
+    const results = cleanResults(await eslint.lintText(`console.log("Hello world!")`, { filePath: 'file.js' }));
 
     expect(results[0].messages).toMatchInlineSnapshot(`
       Array [
@@ -49,13 +55,13 @@ describe('functional', function () {
 
   describe('Vue', function () {
     test('basic lint', async function () {
-      const eslint = new ESLint({
+      const eslint = createESLint({
         baseConfig: generateConfig({
           vue: true,
         }),
       });
 
-      const results = cleanResults(await eslint.lintFiles(path.join(__dirname, 'fixtures', 'component.vue')));
+      const results = cleanResults(await eslint.lintFiles('component.vue'));
 
       expect(results[0].messages).toMatchInlineSnapshot(`
         Array [
@@ -88,6 +94,96 @@ describe('functional', function () {
             "line": 18,
             "message": "Component name \\"v-component\\" is not PascalCase.",
             "ruleId": "vue/component-name-in-template-casing",
+          },
+          Object {
+            "column": 5,
+            "line": 30,
+            "message": "Unexpected console statement.",
+            "ruleId": "no-console",
+          },
+          Object {
+            "column": 17,
+            "line": 30,
+            "message": "Replace \`\\"Hello·world!\\"\` with \`'Hello·world!'\`",
+            "ruleId": "prettier/prettier",
+          },
+        ]
+      `);
+    });
+
+    test('with TypeScript components', async function () {
+      const eslint = createESLint({
+        baseConfig: generateConfig({
+          vue: true,
+          typescript: {
+            vueComponents: true,
+          },
+        }),
+      });
+
+      const results = cleanResults(await eslint.lintFiles('component-typescript.vue'));
+
+      expect(results[0].messages).toMatchInlineSnapshot(`
+        Array [
+          Object {
+            "column": 5,
+            "line": 4,
+            "message": "Component name \\"transition\\" is not PascalCase.",
+            "ruleId": "vue/component-name-in-template-casing",
+          },
+          Object {
+            "column": 29,
+            "line": 5,
+            "message": "Replace \`⏎········my·image:·<img·src=\\"https://example.com/image.png\\"·/>⏎······\` with \`my·image:·<img·src=\\"https://example.com/image.png\\"·/>\`",
+            "ruleId": "prettier/prettier",
+          },
+          Object {
+            "column": 19,
+            "line": 6,
+            "message": "Disallow self-closing on HTML void elements (<img/>).",
+            "ruleId": "vue/html-self-closing",
+          },
+          Object {
+            "column": 28,
+            "line": 13,
+            "message": "Delete \`·\`",
+            "ruleId": "prettier/prettier",
+          },
+          Object {
+            "column": 5,
+            "line": 18,
+            "message": "Component name \\"v-component\\" is not PascalCase.",
+            "ruleId": "vue/component-name-in-template-casing",
+          },
+          Object {
+            "column": 24,
+            "line": 23,
+            "message": "Replace \`}·from·'vue-property-decorator'\` with \`·}·from·'vue-property-decorator';\`",
+            "ruleId": "prettier/prettier",
+          },
+          Object {
+            "column": 55,
+            "line": 23,
+            "message": "Missing semicolon.",
+            "ruleId": "semi",
+          },
+          Object {
+            "column": 3,
+            "line": 27,
+            "message": "Type boolean trivially inferred from a boolean literal, remove type annotation.",
+            "ruleId": "@typescript-eslint/no-inferrable-types",
+          },
+          Object {
+            "column": 3,
+            "line": 29,
+            "message": "Missing return type on function.",
+            "ruleId": "@typescript-eslint/explicit-module-boundary-types",
+          },
+          Object {
+            "column": 10,
+            "line": 29,
+            "message": "Expected 'this' to be used by class method 'mounted'.",
+            "ruleId": "class-methods-use-this",
           },
           Object {
             "column": 5,
